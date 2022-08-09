4095c4a58535fb91253b510be9a5d45b76c0d591
Saifurrahmanemon <saifemon789@gmail.com>
2022-08-10 01:18:10 +0600

    finish with navbar design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import "./App.css";
 import Navbar from "./Pages/Navbar/Navbar";
 
 function App() {
    return (
       <div>
-         <Navbar name="saifur" />
+         <Navbar />
       </div>
    );
 }
--- src/Pages/Navbar/Navbar.tsx
+++ src/Pages/Navbar/Navbar.tsx
@@ -1,10 +1,32 @@
-import { NavbarProps } from "../../types/types";
-
-function Navbar({ name }: NavbarProps) {
+import NavLogo from "../../assets/NavLogo.svg";
+import { NavbarContainer, NavLink, NavLinks } from "./Navbar.styles";
+const navLinks = [
+   {
+      name: "Home",
+      link: "/",
+   },
+   {
+      name: "How it works",
+      link: "/",
+   },
+   {
+      name: "About",
+      link: "/",
+   },
+];
+function Navbar() {
+   const navItems = navLinks.map((item) => (
+      <NavLink key={item.name} href={item.link}>
+         {item.name}
+      </NavLink>
+   ));
    return (
-      <div>
-         <h1>Finish with setup: {name}</h1>
-      </div>
+      <NavbarContainer>
+         <div className="logo">
+            <img src={NavLogo} alt="" />
+         </div>
+         <NavLinks className="navlinks">{navItems}</NavLinks>
+      </NavbarContainer>
    );
 }
 
