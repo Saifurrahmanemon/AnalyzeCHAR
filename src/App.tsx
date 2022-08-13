b38cd12c255eefdda6cb6ce5830c938b0e7c72e9
Saifurrahmanemon <saifemon789@gmail.com>
2022-08-13 19:47:40 +0600

    finish computer vision api config: add all the configuration related to computer vision api two related js files kept in lacal repo not in remote repo because of privacy reason(temp maybe) create basic design for fetched data ImageDetails.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer/Footer";
+import ImageDetails from "./components/ImageDetails/ImageDetails";
 import Navbar from "./Pages/Navbar/Navbar";
 
 function App() {
    return (
       <div>
          <Navbar />
+         <ImageDetails />
          <Footer />
       </div>
    );
